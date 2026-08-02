62ed84a90c2da9f31f45973244b116d970fcdd6c
Hasssan3xl <elhassanehussaine@gmail.com>
2026-08-02 17:12:49 +0100

    added cloudinary storage

--- settings/dev.py
+++ settings/dev.py
@@ -1,6 +1,12 @@
 from .base import *
 import os
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 LOGGING = {
     'version': 1,
